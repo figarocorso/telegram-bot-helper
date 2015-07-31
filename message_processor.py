1123f6a17a0cc51a2bb2a5319381a11b4049429c
Miguel Julián <figarocorso@gmail.com>
2015-07-31 16:07:59 +0200

    Handle new job type: repeated_message
    That only fires a result if a keyword is found *countdown* times
    in a *minutes_timeout* period of time

--- message_processor.py
+++ message_processor.py
@@ -2,6 +2,8 @@ from telegram_bot_helper.api import CommandMessage, UserMessage
 
 from random import randrange
 
+import datetime
+import uuid
 import json
 
 
@@ -9,13 +11,50 @@ class BadJob(Exception):
     pass
 
 
+class JobStatus():
+    job_status = {}
+
+    @classmethod
+    def decrease_countdown(cls, job_id, minutes, countdown):
+        if job_id not in cls.job_status.keys():
+            cls._insert_new_job_status(job_id, minutes, countdown)
+        else:
+            cls.job_status[job_id]['countdown'] -= 1
+
+    @classmethod
+    def _insert_new_job_status(cls, job_id, minutes, countdown):
+        cls.job_status[job_id] = {
+            'countdown': countdown - 1,
+            'timeout': minutes,
+            'creation': datetime.datetime.now()}
+
+    @classmethod
+    def get_countdown(cls, job_id):
+        if job_id not in cls.job_status.keys():
+            return 0
+        return cls.job_status[job_id]['countdown']
+
+    @classmethod
+    def clean(cls):
+        now = datetime.datetime.now()
+        deletions = []
+        for key, status in cls.job_status.iteritems():
+            if (now - status['creation']).seconds / 60 > status['timeout'] or \
+               status['countdown'] <= 0:
+                deletions.append(key)
+
+        for key in deletions:
+            del(cls.job_status[key])
+
+
 class Job():
-    available_types = ['user_message', 'command']
+    available_types = ['user_message', 'command', 'repeated_message']
     available_jobs = ['phrase', 'random_phrase']
 
     def __init__(self, raw_job):
         self.raw_job = raw_job
-        for field in ['keywords', 'message_type', 'job_action', 'data']:
+        for field in ['keywords', 'message_type', 'job_action', 'data',
+                      'minutes_timeout', 'countdown', 'job_id']:
             self.__dict__[field] = getattr(self, "_extract_%s" % field)()
 
     def _extract_keywords(self):
@@ -36,8 +75,16 @@ class Job():
         return job_action
 
     def _extract_data(self):
-        data = self.raw_job.get('data', "")
-        return data
+        return self.raw_job.get('data', "")
+
+    def _extract_countdown(self):
+        return self.raw_job.get('countdown', 0)
+
+    def _extract_minutes_timeout(self):
+        return self.raw_job.get('minutes_timeout', 0)
+
+    def _extract_job_id(self):
+        return self.raw_job.get('job_id', str(uuid.uuid1()))
 
     def matches(self, message):
         if isinstance(message, CommandMessage):
@@ -54,6 +101,17 @@ class Job():
             if word.lower() not in text.lower():
                 return False
         return True
+
+    def should_be_triggered(self):
+        if not self.countdown:
+            return True
+
+        JobStatus.decrease_countdown(self.job_id, self.minutes_timeout,
+                                     self.countdown)
+        trigger = JobStatus.get_countdown(self.job_id) <= 0
+        JobStatus.clean()
+
+        return trigger
 
     def result(self):
         if self.job_action == 'random_phrase':
@@ -72,7 +130,11 @@ class MessageProcessor():
 
     def get_answer(self, message):
         jobs = self._find_matching_jobs(message)
-        return jobs[0].result() if len(jobs) else ''
+        triggered_jobs = [x for x in jobs if x.should_be_triggered()]
+
+        if not len(triggered_jobs):
+            return ''
+        return triggered_jobs[0].result()
 
     def _find_matching_jobs(self, message):
         return [job for job in self.jobs if job.matches(message)]
